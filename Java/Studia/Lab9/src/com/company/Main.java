3c14792e8505f5ddb028a57bf0f6cd17bc03d1d5
Paszta <pasztelana@gmail.com>
2020-06-07 20:11:57 +0200

    studia

--- Java/Studia/Lab9/src/com/company/Main.java
+++ Java/Studia/Lab9/src/com/company/Main.java
@@ -4,13 +4,12 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 
 public class Main extends JFrame implements ActionListener, ItemListener, ChangeListener {
 
+
+    //Declaration
     private JLabel name_l = new JLabel("Name: ");
     private JLabel gender_l = new JLabel("Gender: ");
     private JLabel age_l = new JLabel("Age: ");
@@ -37,7 +36,10 @@ public class Main extends JFrame implements ActionListener, ItemListener, Change
     private JButton confirm = new JButton("Confirm");
     private JButton cancel = new JButton("Cancel");
 
+    private JPanel survey_p = new JPanel();
+    private JLabel survey_l = new JLabel("Do you want to participate in the survey?");
 
+//Constructor
     public Main(){
         super.setTitle("Ankieta");
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -94,6 +96,9 @@ public class Main extends JFrame implements ActionListener, ItemListener, Change
         super.add(confirm);
         super.add(cancel);
 
+        confirm.setBackground(Color.orange);
+        cancel.setBackground(Color.orange);
+
 
 //Positioning
 
@@ -144,7 +149,7 @@ public class Main extends JFrame implements ActionListener, ItemListener, Change
         layout.putConstraint(SpringLayout.WEST, cancel, 20 , SpringLayout.EAST, confirm);
         layout.putConstraint(SpringLayout.NORTH,cancel, 35 , SpringLayout.SOUTH, scroll_pane);
 
-
+//Adding listeners
         surname_tf.addActionListener(this);
         name_tf.addActionListener(this);
         female_button.addItemListener(this);
@@ -152,7 +157,70 @@ public class Main extends JFrame implements ActionListener, ItemListener, Change
         age_s.addChangeListener(this);
 
 
-        super.setVisible(true);
+        confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Integer ageValue = (Integer)age_s.getValue();
+
+                JRadioButton selected_rb = null;
+                if (female_button.isSelected()) {
+                   selected_rb = female_button;
+                }
+                else if (male_button.isSelected()) {
+                    selected_rb = male_button;
+                }
+
+                String pet = (String) pets_list.getSelectedValue();
+
+                JOptionPane.showMessageDialog(confirm, "Name: " + name_tf.getText() + "\n" + "Surname: " + surname_tf.getText() +"\n"+"Age: " + ageValue + "\n" + "Gender: " + selected_rb.getText() +"\n"+ "Pet at home: " + pet + "\n" );
+            }
+        });
+
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(cancel, "Action canceled");
+                System.exit(0);
+            }
+        });
+
+
+            survey_p.setSize(new Dimension(200,200));
+            survey_p.add(survey_l);
+            int option = JOptionPane.showConfirmDialog(null, survey_p, "Survey", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            if(option == 0){
+                super.setVisible(true);
+            }
+            else{
+                System.exit(0);
+            }
+// Mouse adapter class
+        class Adapter extends MouseAdapter {
+              private JButton button;
+
+            public Adapter(JButton button) {
+                this.button = button;
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                button.setBackground(Color.pink);
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                button.setBackground(Color.orange);
+
+            }
+        }
+
+            Adapter adapt_conf = new Adapter(confirm);
+            Adapter adapt_canc = new Adapter(cancel);
+            confirm.addMouseListener(adapt_conf);
+            cancel.addMouseListener(adapt_canc);
     }
 
 
